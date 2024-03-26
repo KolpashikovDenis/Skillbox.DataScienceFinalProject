5677a3c4e276ddc3e23f59928b32de6ec50c42d3
KolpashchikovaNatalia <nakolpash@mail.ru>
2024-03-26 21:18:49 +0400

    Updated pipeline, baseline, models and service

---
+++ myapp/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from myapp.model import Model
+
+# load model
+model = Model()
+
+class Form(BaseModel):
+    index: int
+    session_id: str
+    client_id: str
+    visit_date: str
+    visit_time: str
+    visit_number: int
+    utm_source: str
+    utm_medium: str
+    utm_campaign: str
+    utm_adcontent: str
+    utm_keyword: str
+    device_category: str
+    device_os: str
+    device_brand: str
+    device_model: str
+    device_screen_resolution: str
+    device_browser: str
+    geo_country: str
+    geo_city: str
+
+# app
+app = FastAPI(title='Symbol detection')
+
+# api
+@app.post('/predict')
+def predict(form: Form):
+    df = pd.DataFrame.from_dict([form.dict()])
+    pred = int(model.predict(df)[0])
+    return {'Prediction': pred}
+
+@app.get('/status')
+def status():
+    return "I'm alive"
---
+++ myapp/model.py
@@ -0,0 +1,41 @@
+import os
+import pickle
+import numpy as np
+
+class Model:
+    def __init__(self):
+        model_path = os.path.join('models', 'forest.pkl')
+        source_path = os.path.join('models', 'source.pkl')
+        medium_path = os.path.join('models', 'medium.pkl')
+        campaign_path = os.path.join('models', 'campaign.pkl')
+        adcontent_path = os.path.join('models', 'content.pkl')
+        os_path = os.path.join('models', 'os.pkl')
+        city_path = os.path.join('models', 'city.pkl')
+        
+        # your code here
+        with open(model_path, 'rb') as f:
+            self.model = pickle.load(f)
+        with open(source_path, 'rb') as f:
+            self.source = pickle.load(f)
+        with open(medium_path, 'rb') as f:
+            self.medium = pickle.load(f)
+        with open(campaign_path, 'rb') as f:
+            self.campaign = pickle.load(f)
+        with open(adcontent_path, 'rb') as f:
+            self.adcontent = pickle.load(f)
+        with open(os_path, 'rb') as f:
+            self.os = pickle.load(f)
+        with open(city_path, 'rb') as f:
+            self.city = pickle.load(f)
+           
+
+
+    def predict(self, x):
+        x['utm_source']=self.source.loc[self.source['utm_source']==x['utm_source'].iloc[0], 'target_event'].iloc[0]      
+        x['utm_medium']=self.medium.loc[self.medium['utm_medium']==x['utm_medium'].iloc[0],'target_event'].iloc[0]        
+        x['utm_campaign']=self.campaign.loc[self.campaign['utm_campaign']==x['utm_campaign'].iloc[0],'target_event'].iloc[0]
+        x['utm_adcontent']=self.adcontent.loc[self.adcontent['utm_adcontent']==x['utm_adcontent'].iloc[0],'target_event'].iloc[0]
+        x['device_os']=self.os.loc[self.os['device_os']==x['device_os'].iloc[0],'target_event'].iloc[0]
+        x['geo_city']=self.city.loc[self.city['geo_city']==x['geo_city'].iloc[0],'target_event'].iloc[0]
+        code = self.model.predict(x[['utm_source', 'utm_medium', 'utm_campaign', 'utm_adcontent', 'device_os', 'geo_city']])
+        return code
